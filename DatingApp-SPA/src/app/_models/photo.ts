039f8ac75199ab0d5b13ec3be42758d17b382998
elranso <elranso@gmail.com>
2020-08-11 17:11:51 +0300

    initial commit

---
+++ DatingApp-SPA/src/app/_models/photo.ts
@@ -0,0 +1,9 @@
+import { NumberValueAccessor } from "@angular/forms";
+
+export interface Photo {
+  id: number;
+  url: string;
+  description: string;
+  dateAdded: Date;
+  isMain: boolean;
+}
---
+++ DatingApp.API/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
